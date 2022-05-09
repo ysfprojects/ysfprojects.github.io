9c543d1b5867750b30d58059b187ce90ee48539a
ysfprojects <92296443+ysfprojects@users.noreply.github.com>
2022-05-09 20:19:51 +0200

    third release

--- script.js
+++ script.js
@@ -1,10 +1,115 @@
 function change1() {
-var url = "image/muse.jpg";
+var url = "image/pyra.jpg";
  
 var div = document.getElementById('backchange');
 div.style.backgroundImage = `url(${url})`;
-document.getElementById('textundernav').innerText = "museums of Egypt";
-
-
-
+document.getElementById('textundernav').innerText = "Pyramids of Egypt";
 }
+
+
+function change2() {
+var url = "image/arc.jpg";
+     
+var div = document.getElementById('backchange');
+div.style.backgroundImage = `url(${url})`;
+document.getElementById('textundernav').innerText = "Arc de Triomphe";
+}
+
+
+function change3() {
+    var url = "image/coms.jpg";
+         
+    var div = document.getElementById('backchange');
+    div.style.backgroundImage = `url(${url})`;
+    document.getElementById('textundernav').innerText = "The Colosseum";
+    }
+
+    function change4() {
+        var url = "image/muse.jpg";
+             
+        var div = document.getElementById('backchange');
+        div.style.backgroundImage = `url(${url})`;
+        document.getElementById('textundernav').innerText = "Museums in Egypt";
+        }
+    
+        function change5() {
+            var url = "image/temb.jpg";
+                 
+            var div = document.getElementById('backchange');
+            div.style.backgroundImage = `url(${url})`;
+            document.getElementById('textundernav').innerText = "Temples";
+            }
+
+            function change6() {
+                var url = "image/alex.jpg";
+                     
+                var div = document.getElementById('backchange');
+                div.style.backgroundImage = `url(${url})`;
+                document.getElementById('textundernav').innerText = "Alexandria";
+                }
+
+
+                function change7() {
+                    var url = "image/eiffel.jpg";
+                         
+                    var div = document.getElementById('backchange');
+                    div.style.backgroundImage = `url(${url})`;
+                    document.getElementById('textundernav').innerText = "Eiffel Tower";
+                    }
+
+
+
+
+                    function change8() {
+                        var url = "image/tokyo.jpg";
+                             
+                        var div = document.getElementById('backchange');
+                        div.style.backgroundImage = `url(${url})`;
+                        document.getElementById('textundernav').innerText = "Tokyo";
+                        }
+
+
+                        function change9() {
+                            var url = "image/hang.jpg";
+                                 
+                            var div = document.getElementById('backchange');
+                            div.style.backgroundImage = `url(${url})`;
+                            document.getElementById('textundernav').innerText = "Iraq";
+
+                            }    
+                            
+                            
+
+
+
+                            function change10() {
+                                var url = "image/pales.jpg";
+                                     
+                                var div = document.getElementById('backchange');
+                                div.style.backgroundImage = `url(${url})`;
+                                document.getElementById('textundernav').innerText = "Palestine";
+                                
+                                }           
+                                
+                                
+
+
+                                function change11() {
+                                    var url = "image/china.jpg";
+                                         
+                                    var div = document.getElementById('backchange');
+                                    div.style.backgroundImage = `url(${url})`;
+                                    document.getElementById('textundernav').innerText = "The Great Wall of China";
+                                    
+                                    }
+                                    
+                                    
+
+                                    function change12() {
+                                        var url = "image/pisa.jpg";
+                                             
+                                        var div = document.getElementById('backchange');
+                                        div.style.backgroundImage = `url(${url})`;
+                                        document.getElementById('textundernav').innerText = "pisa Tower";
+                                        
+                                        }              
