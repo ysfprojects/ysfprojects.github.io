8aacd0a502fe0a73600c0f2a9707e8a1ba5d3445
ysfprojects <92296443+ysfprojects@users.noreply.github.com>
2022-05-09 07:24:15 +0200

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+function change1() {
+var url = "image/muse.jpg";
+ 
+var div = document.getElementById('backchange');
+div.style.backgroundImage = `url(${url})`;
+document.getElementById('textundernav').innerText = "museums of Egypt";
+
+
+
+}
